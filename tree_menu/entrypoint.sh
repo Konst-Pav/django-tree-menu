e18687ab7a91a8da5a02a82725d086d9a55d4fcb
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-10 17:31:34 +0300

    Created dockerfile, compose.yaml, entrypoint.sh

---
+++ tree_menu/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python manage.py makemigrations
+python manage.py migrate
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username "$DJANGO_SUPERUSER_USERNAME"
+fi
