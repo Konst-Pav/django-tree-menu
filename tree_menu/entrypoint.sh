5908a1e800f56e955a3c59fd8f4c6edd289b8b2a
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-11 16:28:07 +0300

    Added create_superuser command

--- tree_menu/entrypoint.sh
+++ tree_menu/entrypoint.sh
@@ -2,8 +2,4 @@
 
 python manage.py makemigrations
 python manage.py migrate
-
-if [ "$DJANGO_SUPERUSER_USERNAME" ]
-then
-    python manage.py createsuperuser --noinput
-fi
+python manage.py create_superuser
