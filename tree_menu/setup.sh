68e5030b2efd3e54ca582da7a97f09c44490103d
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-11 18:21:31 +0300

    Update setup.sh

--- tree_menu/setup.sh
+++ tree_menu/setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if ! [ -f ".env" ]; then
-  cat .env.example > .env
-fi
-
-while IFS='=' read -r key value; do
-	export "$key"="$value"
-done < ".env"
-
-
 python manage.py makemigrations
 python manage.py migrate
 python manage.py create_superuser
