6313e4cb1f234b07278e3736d903bb06b828e99a
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-11 16:27:33 +0300

    Created create_superuser.py in management/commands

---
+++ tree_menu/app/management/commands/create_superuser.py
@@ -0,0 +1,24 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+
+UserModel = get_user_model()
+
+SUPERUSER_USERNAME = os.getenv('DJANGO_SUPERUSER_USERNAME', default='admin')
+SUPERUSER_EMAIL = os.getenv('DJANGO_SUPERUSER_EMAIL', default='admin@admin.mail')
+SUPERUSER_PASSWORD = os.getenv('DJANGO_SUPERUSER_PASSWORD', default='admin')
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser when the application is launched'
+
+    _class = UserModel
+
+    def handle(self, *args, **options):
+        self._class.objects.create_superuser(
+            SUPERUSER_USERNAME,
+            SUPERUSER_EMAIL,
+            SUPERUSER_PASSWORD,
+        )
