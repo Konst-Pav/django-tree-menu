e7290fd77aedfdfc14081292dd15f26aab02d005
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-15 12:16:50 +0300

    Added MenuView

--- tree_menu/app/views.py
+++ tree_menu/app/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.list import ListView
+from django.views import View
+from django.shortcuts import render
 from app.models import Menu
 
 
@@ -6,3 +8,16 @@ class MenuListView(ListView):
     model = Menu
     template_name = 'index.html'
     context_object_name = 'menu'
+    queryset = Menu.objects.filter(parent_id=None)
+
+
+class MenuView(View):
+
+    def get(self, request, *args, **kwargs):
+        menu_slug = kwargs.get('menu_slug')
+        menu_path = kwargs.get('menu_path')
+
+        return render(
+            request,
+            'catalog.html',
+            {'menu_slug': menu_slug, 'menu_path': menu_path})
