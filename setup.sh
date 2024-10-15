b297bd7eb22aaa1b96276df887a74622a0862325
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-15 18:31:38 +0300

    Added create data to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ fi
 python tree_menu/manage.py makemigrations
 python tree_menu/manage.py migrate
 python tree_menu/manage.py create_superuser
+python tree_menu/manage.py create_data
 python tree_menu/manage.py collectstatic --no-input
