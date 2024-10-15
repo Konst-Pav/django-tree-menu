b0490795e532d475ff977997c75f2c5dfcf44010
kpavlov <k.pavlov.reg@yandex.ru>
2024-10-15 12:16:26 +0300

    Added menu paths

--- tree_menu/tree_menu/urls.py
+++ tree_menu/tree_menu/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import MenuListView
+from app.views import MenuListView, MenuView
 
-urlpatterns = [
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+
+urlpatterns = debug_toolbar_urls() + [
     path('admin/', admin.site.urls),
-    path('', MenuListView.as_view(), name='menu_list')
+    path('', MenuListView.as_view(), name='menu_list'),
+    path('<slug:menu_slug>/', MenuView.as_view()),
+    path('<slug:menu_slug>/<path:menu_path>/', MenuView.as_view()),
 ]
